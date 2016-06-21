58d4f35b17c897df05148820489b0ffc25507d50
Ibnu Daru Aji <ibnu.d.a@student.uns.ac.id>
2016-06-22 00:21:21 +0700

    Fixed some unit tests.

--- Kafe.Core.Tests/OpenTabTests.fs
+++ Kafe.Core.Tests/OpenTabTests.fs
@@ -7,11 +7,21 @@ open Events
 open Commands
 open States
 open System
+open Errors
 
 [<Test>]
 let ``Can Open a new Tab`` () =
   let tab = {Id = Guid.NewGuid(); TableNumber = 1}
+
   Given (ClosedTab None)
   |> When (OpenTab tab)
   |> ThenStateShouldBe (OpenedTab tab)
   |> WithEvents [TabOpened tab]
+
+[<Test>]
+let ``Cannot open an already Opened Tab`` () =
+  let tab = {Id = Guid.NewGuid(); TableNumber = 1}
+
+  Given (OpenedTab tab)
+  |> When (OpenTab tab)
+  |> ShouldFailWith TabAlreadyOpened
--- Kafe/CommandHandlers.fs
+++ Kafe/CommandHandlers.fs
@@ -1,17 +1,22 @@
 ﻿module CommandHandlers
 
+open Chessie.ErrorHandling
+open Commands
 open States
 open Events
-open System
-open Commands
-open Domain
+open Errors
 
 let execute state command =
   match command with
-  | OpenTab tab -> TabOpened tab
-  | _ -> failwith "TODO" // [TabOpened { Id = Guid.NewGuid(); TableNumber = 1}]
+  | OpenTab tab ->
+    match state with
+    | ClosedTab _ -> [TabOpened tab] |> ok
+    | _ -> TabAlreadyOpened |> fail
+  | _ -> failwith "Todo"
 
 let evolve state command =
-  let events = execute state command
-  let newState = apply state events
-  (newState, events)
+  match execute state command with
+  | Ok (events, _) ->
+    let newState = List.fold apply state events
+    (newState, events) |> ok
+  | Bad err -> Bad err
